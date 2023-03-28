23cf926b8c5806c1900e26cbe025cd1c293b9d5c
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-03-28 21:19:56 +0100

    Making the Connection(without test )

---
+++ src/main/java/Dbconnection/DbConnection.java
@@ -0,0 +1,62 @@
+package Dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    static Connection connection;
+
+    public static  Connection createconnectiontoOrtho(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // getting the driver
+            // creating the connection with the database
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/orthodontic department?useSSL=false"
+                    ,"root"
+                    ,"Password");
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    public static  Connection createconnectiontoTeethcleaning(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // getting the driver
+            // creating the connection with the database
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Teethcleaning?useSSL=false"
+                    ,"root"
+                    ,"Password");
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    public static  Connection createconnectiontoimplantsandextraction(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // getting the driver
+            // creating the connection with the database
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/implantsandextraction?useSSL=false"
+                    ,"root"
+                    ,"Password");
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+
+}
